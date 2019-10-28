3d650bccdc0cc56a9fe176e2a71d0320cc686d5a
Vitor <vitor-zaions@hotmail.com>
2019-10-28 20:35:23 -0300

    PHP CONNECT SQL SERVER

--- pages/index.php
+++ pages/index.php
@@ -109,6 +109,44 @@
 </div>		
 </div>
 <div class="container">
+
+<?php>
+// Dados do banco
+
+$dbhost   = “hostbanco”;   #Nome do host
+$db       = “nomebanco”;   #Nome do banco de dados
+$user     = “nomeusuario”; #Nome do usuário
+$password = “senhabase”;   #Senha do usuário
+
+// Dados da tabela
+
+$tabela = “nometabela”;    #Nome da tabela
+$campo1 = “campo1tabela”;  #Nome do campo da tabela
+$campo2 = “campo2tabela”;  #Nome de outro campo da tabela
+
+ 
+
+@mssql_connect($dbhost,$user,$password) or die
+(“Não foi possível a conexão com o servidor!”);
+@mssql_select_db(“$db“) or die
+(“Não foi possível selecionar o banco de dados!”); 
+
+$instrucaoSQL = “SELECT $campo1, $campo2 FROM $tabela ORDER BY $campo1“;
+$consulta = mssql_query($instrucaoSQL);
+$numRegistros = mssql_num_rows($consulta); 
+
+echo “Esta tabela contém $numRegistros registros!\n<hr>\n“; 
+
+if ($numRegistros!=0) {
+while ($cadaLinha = mssql_fetch_array($consulta)) {
+echo “$cadaLinha[$campo1] – $cadaLinha[$campo2]\n<br>\n“;
+ }
+}
+
+?>
+
+
+
 <div class="mt-3 mb-4" style="float: left; margin-left : 15px">	
 <div class="card" style="width: 18rem;">
   <img class="card-img-produtos" src="../images/prod_01.jpg" alt="Carvao">
--- scripts/produtos.js
+++ scripts/produtos.js
@@ -3,24 +3,7 @@ window.onload = function() {
 };
 
 function pegarprodutos(){
-
-    var objConnection = new ActiveXObject("adodb.connection");
-    var strConn = "driver={sql server};server=QITBLRQIPL030;database=Norden;uid=sa;password=12345";
-    objConnection.Open(strConn);
-    var rs = new ActiveXObject("ADODB.Recordset");
-    var strQuery = "SELECT * FROM  Person.Address";
-    rs.Open(strQuery, objConnection);
-    rs.MoveFirst();
-    while (!rs.EOF) {
-        document.write(rs.fields(0) + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-        document.write(rs.fields(1) + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;");
-        document.write(rs.fields(2) + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;    ");
-        document.write(rs.fields(3) + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;    ");
-        document.write(rs.fields(4) + "<br/>");
-        rs.movenext();
-    }
-
-
+  
 }
 
 
